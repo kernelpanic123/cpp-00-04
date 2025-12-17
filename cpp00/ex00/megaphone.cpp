f408c308a19e906f815fa953c4dad7c0270335b0
kernelpanic <abdertouaitia@gmail.com>
2025-12-17 02:08:08 +0100

    l

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	std::cout << "hello world !" << std::endl;
+	int j = 0;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++)
+	{
+		while (argv[i][j] != '\0')
+		{
+			std::cout << (char)std::toupper(argv[i][j]);
+			j++;
+		}
+		j = 0;
+	}
+	std::cout << std::endl;
 	return 0;
 }
