572b0e2ba953d06002687213580298e6d07de831
poker_FACE <abdertouaitia@gmail.com>
2026-01-12 18:09:41 +0100

    l

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Error argument, valid expresion: <filename> <s1> <s2> !" << std::endl;
+	}
+}
