ebdf900361daad435e890ba2a12a0a7d7018fd76
Abderahime Touaitia <abtouait@c2r9p3.42nice.fr>
2026-01-26 09:57:20 +0100

    l

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,34 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixed_point_nbr(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+Fixed& Fixed::operator=( const Fixed &src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &src) 
+		_fixed_point_nbr = src.getRawBits();
+	return *this;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_fixed_point_nbr);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_fixed_point_nbr = raw;
+}
